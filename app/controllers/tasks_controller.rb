69c84ba46feeb4d524fafd91e520f91e7181aaf6
remcojonk <remcojonk@hotmail.com>
2022-11-15 14:53:06 +0100

    CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,40 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @task = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    redirect_to task_path(@task) if @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task) if @task.save
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :complete)
   end
 end
